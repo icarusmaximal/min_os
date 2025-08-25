b6840a41850455e028e16397ee7105d407e6fea8
icarusmaximal <icarusmaximal@users.noreply.github.com>
2025-08-25 14:50:18 +0530

    vga text mode implemented

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,41 @@
 #![no_main]
 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 static HELLO: &[u8] = b"Hello World!";
 
+mod vga_buffer;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
+    //
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    //
+    //
+    // vga_buffer::print_test();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
+    vga_buffer::WRITER.lock().write_str("\n").unwrap();
+
+    println!("hello by {}", "println!");
+
     loop {}
 }
